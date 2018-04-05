f92ea3788f3f94846ef2c828f3192ac882fb6018
giuseppe <g.caferra@outlook.com>
2018-04-05 13:55:19 +0200

    Mine first NightFox layout

--- keyboards/whitefox/keymaps/joefox/keymap.c
+++ keyboards/whitefox/keymaps/joefox/keymap.c
@@ -18,12 +18,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define _BL 0
 #define _FL 1
-#define _MAC_BL 2
-#define _MAC_FL 3
-
-#define PERMISSIVE_HOLD 1
-
-#define CTRL_AC LALT(LCTL(KC_DEL))
 
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer
@@ -39,32 +33,22 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |Fn0|Gui |Alt |         Space/Fn0|Fn0 |Alt |Gui |   |Lef|Dow|Rig|
      * `---------------------------------------------------------------'
      */
-    [_BL] = LAYOUT( \
-        KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0, KC_MINS,KC_EQL, KC_GRV, TO(_MAC_BL), KC_INS, \
-        KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSLS,     KC_DEL, \
-        KC_BSPACE,KC_A,ALT_T(KC_S),SFT_T(KC_D),CTL_T(KC_F),KC_G,KC_H,CTL_T(KC_J),SFT_T(KC_K),ALT_T(KC_L),KC_SCLN,KC_QUOT,KC_NUHS,KC_ENT,KC_HOME,\
-        KC_LSHIFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSHIFT,  KC_UP,  KC_END,\
-        KC_RGUI,  KC_RCTRL,  KC_LALT,             LT(_FL,KC_SPC)      ,       KC_RALT,KC_RGUI, KC_TRNS,     KC_LEFT,KC_DOWN,KC_RGHT \
-    ),
-    [_FL] = LAYOUT( \
-        KC_TRNS,KC_F1,KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7, KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS,KC_TRNS,KC_GRAVE,\
-        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_PSCR,\
-        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_VOLU,\
-        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_PGUP,KC_VOLD,\
-        KC_TRNS,KC_TRNS,KC_TRNS,               KC_TRNS,                         KC_TRNS,KC_TRNS,KC_TRNS,     KC_HOME,KC_PGDN,KC_END  \
-    ),
-    [_MAC_BL] = LAYOUT( \
-        KC_ESC, KC_1,   KC_2,   KC_3,   KC_4, KC_5,  KC_6,   KC_7,   KC_8,   KC_9,  KC_0, KC_MINS,KC_EQL, KC_GRV, TO(_BL), KC_INS, \
-        KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC, KC_BSLS,    KC_DEL, \
-        KC_BSPC,KC_A,ALT_T(KC_S),SFT_T(KC_D),CTL_T(KC_F),KC_G,KC_H,CTL_T(KC_J),SFT_T(KC_K),ALT_T(KC_L),KC_SCLN,KC_QUOT,KC_NUHS,KC_ENT,      KC_HOME,\
+    [_BL] = KEYMAP( \
+        KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0, KC_MINS,KC_EQL, KC_GRV, KC_BSLS, KC_INS, \
+        KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC,     KC_DEL, \
+        KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_NUHS,KC_ENT,      KC_HOME,\
         KC_LSFT,KC_NUBS,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,     KC_UP,  KC_END,\
-        KC_RGUI,KC_RCTL, KC_LALT,             LT(_MAC_FL,KC_SPC)   ,       KC_APP,KC_RGUI, KC_TRNS,     KC_LEFT,KC_DOWN,KC_RGHT \
+        KC_FN0,KC_LGUI,KC_LALT,             LT(_FL,KC_SPC)      ,           KC_RALT,KC_RCTL, KC_RCTL,     KC_LEFT,KC_DOWN,KC_RGHT \
     ),
-    [_MAC_FL] = LAYOUT( \
-        KC_ESC,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS,KC_TRNS,KC_GRAVE,\
-        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PSCR,KC_SLCK,KC_PAUS,KC_TRNS,     KC_PSCR,\
+    [_FL] = KEYMAP( \
+        KC_ESC,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, KC_TRNS,KC_TRNS,KC_MUTE,\
+        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_PSCR,KC_SLCK,KC_PAUS,KC_TRNS,     KC_TRNS,\
         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_VOLU,\
         KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,     KC_PGUP,KC_VOLD,\
         KC_TRNS,KC_TRNS,KC_TRNS,               KC_TRNS,                         KC_TRNS,KC_TRNS,KC_TRNS,     KC_HOME,KC_PGDN,KC_END  \
     ),
+};
+
+const uint16_t fn_actions[] = {
+    [0] = ACTION_LAYER_MOMENTARY(_FL),
 };
