69cf441e8b9b12875e56f2d1764b871ca93bc020
giuseppe <g.caferra@outlook.com>
2018-04-10 14:09:28 +0200

    added Ingnore_mod_tap_interrupt flag

--- keyboards/whitefox/config.h
+++ keyboards/whitefox/config.h
@@ -70,4 +70,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 #define TAPPING_TERM 500
+#define IGNORE_MOD_TAB_INTERRUPT
 #endif
